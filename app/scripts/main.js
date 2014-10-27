13ab6b7098d5872756daab6971a49a11ff06bc5c
Matt Griffeth <mgriffeth9@gmail.com>
2014-10-27 14:08:02 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+console.log("{mustachioed mofos}");
+
+var collection = {
+  comics: [
+  {title:"The Killing Joke",company:"DC"},
+  {title:"The Death of Superman",company:"DC"},
+  {title:"Deadpool Kills",company:"Marvel"},
+  {title:"Hellboy ",company:"Dark Horse"},
+  {title:"Battle Chasers",company:"Image"},
+  {title:"Ninjak",company:"Valiant"}
+  ]};
